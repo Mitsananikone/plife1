29985d5b844bd9b19d64e0633b76c8d331310668
Mit Sananikone <Mitsananikone@gmail.com>
2023-08-28 14:39:30 -0500

    Update index.js
    changed MotionTest to LandingPage

--- pages/index.js
+++ pages/index.js
@@ -18,8 +18,8 @@ export default function Home() {
             {/* <ProcedureTypes/> */}
             {/* <CosmeticSurgeryPage/>
 <BreastImplantInfo/> */}
-            <MotionTest/>
-            {/* <LandingPage /> */}
+            // <MotionTest/>
+             <LandingPage /> 
      
             {/* <AboutPage/>
             <ProcedurePage/> */}
