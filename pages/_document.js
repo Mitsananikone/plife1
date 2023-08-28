7fe4bf9d1935984bcd144d1642277cc703f3c218
Mit Sananikone <Mitsananikone@gmail.com>
2023-08-28 15:07:07 -0500

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,40 +1,24 @@
-import Document, { Html, Main, NextScript } from 'next/document';
-import Head from 'next/head';
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
     static async getInitialProps(ctx) {
-        const initialProps = await Document.getInitialProps(ctx);
-        return { ...initialProps };
+        const initialProps = await Document.getInitialProps(ctx)
+        return { ...initialProps }
     }
 
     render() {
         return (
             <Html lang="en">
                 <Head>
-                    <script
-                        dangerouslySetInnerHTML={{
-                            __html: `
-                                window.markerConfig = {
-                                    project: '64ecfc1f8698fbbbb8c637e4',
-                                    source: 'snippet'
-                                };
-                            `
-                        }}
-                    />
-                    <script
-                        dangerouslySetInnerHTML={{
-                            __html: `
-                                !function(e,r,a){if(!e.__Marker){e.__Marker={};var t=[],n={__cs:t};["show","hide","isVisible","capture","cancelCapture","unload","reload","isExtensionInstalled","setReporter","setCustomData","on","off"].forEach(function(e){n[e]=function(){var r=Array.prototype.slice.call(arguments);r.unshift(e),t.push(r)}}),e.Marker=n;var s=r.createElement("script");s.async=1,s.src="https://edge.marker.io/latest/shim.js";var i=r.getElementsByTagName("script")[0];i.parentNode.insertBefore(s,i)}}(window,document);
-                            `
-                        }}
-                    />
+                    {/* You can put any document-wide meta tags here */}
                 </Head>
                 <body>
                     <Main />
                     <NextScript />
+                    
                 </body>
             </Html>
-        );
+        )
     }
 }
 
